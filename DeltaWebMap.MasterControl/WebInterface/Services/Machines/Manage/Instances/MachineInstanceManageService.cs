145526cbd865ee76419a20a29fe9ff54b92a9269
RomanPort <34401219+Roman-Port@users.noreply.github.com>
2020-12-08 10:02:26 -0600

    Improved style

--- DeltaWebMap.MasterControl/WebInterface/Services/Machines/Manage/Instances/MachineInstanceManageService.cs
+++ DeltaWebMap.MasterControl/WebInterface/Services/Machines/Manage/Instances/MachineInstanceManageService.cs
@@ -56,7 +56,7 @@ namespace DeltaWebMap.MasterControl.WebInterface.Services.Machines.Manage.Instan
             }
 
             //Log
-            WriteString($"<div style=\"display:flex;margin-bottom:5px;\"><div style=\"margin-right: 10px;white-space: nowrap;\"><span style=\"background-color:{color};color:white;\">[{logLevel.ToString().ToUpper()}]</span> <u style=\"color:{color};\">{HttpUtility.HtmlEncode(topic)}</u></div><div>{HttpUtility.HtmlEncode(message)}</div></div>");
+            WriteString($"<div style=\"display:flex;margin-top:5px;\"><div style=\"margin-right: 10px;white-space: nowrap;\"><span style=\"background-color:{color};color:white;border-radius: 5px;padding: 3px 6px;\">{logLevel.ToString().ToUpper()}</span> <u style=\"color:{color};\">{HttpUtility.HtmlEncode(topic)}</u></div><div>{HttpUtility.HtmlEncode(message)}</div></div>");
         }
     }
 
